00e1bb19b5601055e30a43494df71b2c211a5eba
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 21:09:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
-def reverse_word_order(sentence)
+def reverse_each_word(sentence)
   reversed = []
   sentence.split.each do |word|
     reversed.unshift word
   end
   reversed.join(' ')
 end
+
 puts reverse_each_word("alex, is cool")
