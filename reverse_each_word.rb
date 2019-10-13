e5ba12663e4267047d268c4211e293a4859dc708
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 21:15:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(sentence)
   reversed = []
+  reversed_word = ""
   sentence.split.collect do |word|
-    word.split.collect{ |char|  }
+    word.split.collect{ |char|
+      reversed.reverse.push(char)
+    }
   end
 end
 
