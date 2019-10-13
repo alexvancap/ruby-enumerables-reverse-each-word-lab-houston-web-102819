5c574c3bcd83f71bbfe9972f2dc828db83a5beaf
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 20:55:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   split_string = string.split("")
   reversed = []
-  string.size.times { reversed << split_string.pop }
+  sentence.size.times { reversed << split_string.pop }
   reversed.join
 end
 
