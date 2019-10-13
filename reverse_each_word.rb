aa037bdf4642cf6feb21111a186d7117c7c96178
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 20:25:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split(',')
-  
+
   new_sentence.each{|p| p.reverse}
 
   new_sentence
 end
 
-reverse_each_word("gollo hdrghfs")
+puts reverse_each_word("gollo hdrghfs")
