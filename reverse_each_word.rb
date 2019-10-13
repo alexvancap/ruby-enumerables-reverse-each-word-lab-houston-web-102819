41a97f2911ce1370d96c9a7f9a6f7efa62e9eb09
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 20:41:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
-def reverse_each_word(sentence)
-  return s[::-1]
-end
+def reverse_slicing(s):
+    return s[::-1]
+
+input_str = 'ABç∂EF'
+
+if __name__ == "__main__":
+    print('Reverse String using slicing =', reverse_slicing(input_str))
 
 puts reverse_each_word("alex, is cool")
