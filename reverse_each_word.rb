4a93260330e7fcd05fab041d2917bf73e6af557f
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 20:57:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(sentence)
  string = sentence.split('')
  reversed_string = []
-
+ 
  string.each do |char|
   reversed_string.unshift(char)
  end
-
+ 
  return reversed_string.join('')
 end
 
