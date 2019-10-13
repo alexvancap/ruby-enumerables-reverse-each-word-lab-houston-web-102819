9c2a4df9cb5b8309bb20fa24c5a8b0c0a74dfb8a
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 20:52:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   new_sentence = sentence.split('')
   reversed = []
 
-  new_sentence.each{|s| reversed << new_sentence.pop(s)}
+  new_sentence.each{|s| reversed << new_sentence.pop()}
   return new_sentence
 end
 
